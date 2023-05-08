3d5848865664f36fe8aacd9db4393b0336ecb7ab
Dan Butcher <danbobbutcher@gmail.com>
2023-05-08 15:24:24 -0600

    Added dynamically changing background colors to cards

--- app/src/pages/components/MainContent.tsx
+++ app/src/pages/components/MainContent.tsx
@@ -1,12 +1,22 @@
-import ContentCard from "@/pages/components/ContentCard";
+import ContentCard from "@/pages/components/ContentCard/ContentCard";
 
 export default function MainContent({}){
 
+    const content : JSX.Element[] = [
+        <><p className={"content-text responsive"}>Content 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam distinctio est ex minima mollitia necessitatibus numquam omnis perspiciatis quisquam. Ipsa iste, maiores maxime quia quos reiciendis sequi similique sunt vitae?</p></>,
+        <><p className={"content-text responsive"}>Content 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam distinctio est ex minima mollitia necessitatibus numquam omnis perspiciatis quisquam. Ipsa iste, maiores maxime quia quos reiciendis sequi similique sunt vitae?</p></>,
+        <><p className={"content-text responsive"}>Content 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam distinctio est ex minima mollitia necessitatibus numquam omnis perspiciatis quisquam. Ipsa iste, maiores maxime quia quos reiciendis sequi similique sunt vitae?</p></>,
+    ]
+
     return(
         <div className={"content-frame"}>
-            <ContentCard index={0}>
-                <p className={"content-text responsive"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam distinctio est ex minima mollitia necessitatibus numquam omnis perspiciatis quisquam. Ipsa iste, maiores maxime quia quos reiciendis sequi similique sunt vitae?</p>
-            </ContentCard>
+            {content.map((value: JSX.Element, index: number):  JSX.Element =>{
+                return (
+                    <ContentCard index={index}>
+                        {value}
+                    </ContentCard>
+                )
+            })}
         </div>
     )
 }
