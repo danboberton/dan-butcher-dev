343edda91f6973fb5de5f797e364a6fef5fc7fb2
Dan <danbobbutcher@gmail.com>
2023-05-26 17:36:52 -0600

    moved title to _app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
+import {Head} from "next/document";
 
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <>
+            <Head>
+                <title>Dan Butcher is a dev</title>
+            </Head>
             <Component {...pageProps} />
         </>
     )
