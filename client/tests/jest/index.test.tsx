ae01a0fff2e0844aaf76775e500107cd63db64df
Dan <danbobbutcher@gmail.com>
2023-05-29 15:18:49 -0600

    trying out auto-testing config

--- client/tests/jest/index.test.tsx
+++ client/tests/jest/index.test.tsx
@@ -1,7 +1,7 @@
 import {render, screen} from '@testing-library/react'
 import Home from '@/pages/index'
 
-describe('Home', () => {
+describe('Home renders basic components', () => {
     beforeEach(() => {
         render(<Home/>)
     })
