fa90657e8d4c0142589ff5b024ef00be15c12dbd
Dan <danbobbutcher@gmail.com>
2023-05-30 22:16:34 -0600

    removed unused import

--- client/src/components/Header/NavBar/Buttons/NavButton.tsx
+++ client/src/components/Header/NavBar/Buttons/NavButton.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "@/components/Header/NavBar/Buttons/NavButton.module.css";
-import {NavBarContext} from "@/components/Header/NavBar/NavBar";
 
 type Props = {
     title: string,
