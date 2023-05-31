ffc89368e5167e451cc721389f87db87f8a84931
Dan <danbobbutcher@gmail.com>
2023-05-30 21:49:08 -0600

    refactored NavBar with create Portal
    used createPortal to position the sidebar element close #32
    removed useless design directory close #28

--- client/src/components/Header/NavBar/NavBar.tsx
+++ client/src/components/Header/NavBar/NavBar.tsx
@@ -1,15 +1,97 @@
-import styles from "./NavBar.module.css"
-import React from "react";
+import React, {useEffect, useState} from "react";
+import NavButtonsAll from "@/components/Header/NavBar/NavButtonsAll";
+import SideBar from "@/components/Header/NavBar/Buttons/SideBar";
+import HamburgerButton from "@/components/Header/NavBar/Buttons/HamburgerButton";
+import styles from "@/components/Header/NavBar/NavBar.module.css";
+import {createPortal} from "react-dom";
+
+export default function NavBar(): JSX.Element {
+    const [shouldUseSideBar, setShouldUseNavBar] = useState(false)
+    const [sideBarActive, setSideBarActive] = useState(false)
+
+    const SIDEBAR_THRESHOLD: number = 600
 
 
-interface Props {
-    children: JSX.Element
+    useEffect(() => {
+        const isNavHamburgerThreshold = (): boolean => {
+            return window.innerWidth <= SIDEBAR_THRESHOLD
+        }
+        const handleResize = (): void => {
+            const isWidthThreshold = isNavHamburgerThreshold()
+
+            if (!isWidthThreshold) setSideBarActive(false)
+            setShouldUseNavBar(isWidthThreshold)
+        }
+        window.addEventListener("resize", handleResize)
+        handleResize()
+        return () => window.removeEventListener("resize", handleResize)
+    }, [])
+
+    const result = () => {
+        if (!shouldUseSideBar) {
+            return (
+                <div className={styles.navContainer}>
+                    <NavButtonsAll setSideBarActive={false}/>
+                </div>
+            )
+        } else {
+            const sideBar =
+                <SideBar sideBarActive={sideBarActive}>
+                    <NavButtonsAll setSideBarActive={setSideBarActive}/>
+                </SideBar>
+            const sidebarPlacement = () => {
+                const mainContainer = document.getElementById('main-container')
+                return mainContainer ? createPortal(sideBar, mainContainer) : null
+            }
+
+            return (
+                <div className={styles.navContainer}>
+                    <HamburgerButton sideBarActive={sideBarActive}
+                                     setSideBarActive={setSideBarActive}/>
+                    {sidebarPlacement()}
+                </div>
+            )
+        }
+    }
+
+    return result()
 }
-export default function NavBar({children}: Props): JSX.Element {
 
-    return (
-        <div className={styles.navContainer}>
-            {children}
-        </div>
-    )
-}
+
+// export const UseSideBar = ({navBarContext}: { navBarContext: NavBarContext }): JSX.Element => {
+//     return (
+//         <SideBar sideBarActive={navBarContext.sideBarActive}>
+//             <NavButtonsAll navBarContext={navBarContext}/>
+//         </SideBar>
+//     )
+// }
+//
+// export const UseTopNavBar = (): JSX.Element => {
+//
+//     return (
+//         <NavBar>
+//             <NavButtonsAll navBarContext={false}/>
+//         </NavBar>
+//     )
+// }
+//
+// export const HamburgerInTopNavBar = ({navBarContext}: { navBarContext: NavBarContext }): JSX.Element => {
+//
+//     return (
+//         <NavBar>
+//             <HamburgerButton sideBarActive={navBarContext.sideBarActive}
+//                              setSideBarActive={navBarContext.setSideBarActive}/>
+//         </NavBar>
+//     )
+// }
+//
+// export const TopNavOrHamburger = ({navBarContext}: { navBarContext: NavBarContext }): JSX.Element => {
+//
+//     return !navBarContext.shouldUseSideBar ? <UseTopNavBar/> :
+//         <HamburgerInTopNavBar navBarContext={navBarContext}/>
+// }
+//
+// export const Sidebar = ({navBarContext}: { navBarContext: NavBarContext }): JSX.Element => {
+//
+//     return navBarContext.shouldUseSideBar ? <UseSideBar navBarContext={navBarContext}/> : <></>
+// }
