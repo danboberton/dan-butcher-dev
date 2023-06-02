a8db217a92a97a38f65636b2f3e7a35470373942
Dan Butcher <danbobbutcher@gmail.com>
2023-06-02 15:58:42 -0600

    wrapper working

--- client/tests/jest/api/getUser.test.ts
+++ client/tests/jest/api/getUser.test.ts
@@ -3,8 +3,8 @@ import {getUser} from "@/lib/database/getUser";
 describe('Able to get a user',  ()=>{
 
     it('gets a user', async()=>{
-        const user = await getUser('user')
+        const user = await getUser('fakepassword')
         console.log(JSON.stringify(user))
-        expect(user).toEqual('user')
+        expect(user.user).toEqual('fakepassword')
     })
 })
