ca8208a2227b6be914e93b16a91b92d931ad3caf
Dan Butcher <danbobbutcher@gmail.com>
2023-06-05 12:45:39 -0600

    headshot added, looks good

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -11,15 +11,24 @@ export default function About(): JSX.Element {
             <ContentHeader title={"Hi, I'm Dan!"} icon={<FaGrav/>}/>
             <div className={styles.aboutContainer}>
                 <div className={styles.headshotContainer}>
-                    <Image src={'/headshot.jpeg'} alt={'Headshot of Dan'} width={100} height={100}/>
+                    <div className={styles.shadow}>
+                        <Image src={'/headshot-scaled.jpeg'} alt={'Headshot of Dan'} width={300} height={100}
+                               className={styles.headshot}/>
+                    </div>
                 </div>
-                <p>I wrote my first code when I was 8 years old, copying some BASIC
-                    from my favorite magazine, 3-2-1 Contact. I have been obsessed with technology ever since. In my {""}
-                    previous career I enjoyed a successful decade in the music and video industry, working with {""}
-                    talented  artists and state of the art technology. This high pressure work environment taught me to {""}
-                    troubleshoot under pressure and work successfully in team environments to achieve our goals in {""}
-                    extremely diverse conditions. It also forged my deep appreciation of the intersection of technology {""}
-                    and art. <em>I love designing and building solutions to problems.</em></p>
+                <div className={styles.me}>
+                    <p>&ensp; I wrote my first code when I was 8 years old, copying some BASIC
+                        from my favorite magazine, 3-2-1 Contact. I have been obsessed with technology ever since. In
+                        my {""}
+                        previous career I enjoyed a successful decade in the music and video industry, working with {""}
+                        talented artists and state of the art technology. This high pressure work environment taught me
+                        to {""}
+                        troubleshoot under pressure and work successfully in team environments to achieve our goals
+                        in {""}
+                        extremely diverse conditions. It also forged my deep appreciation of the intersection of
+                        technology {""}
+                        and art. <em>I love designing and building solutions to problems.</em></p>
+                </div>
             </div>
 
         </div>
