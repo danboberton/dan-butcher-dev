cd5b82b3ee5ee2a2f37a1c30ffb5d15119ff1a37
Dan <danbobbutcher@gmail.com>
2023-06-14 06:59:59 -0600

    tests for withDatabaseConnection

--- client/tests/jest/lib/database/withDatabaseConnection.test.ts
+++ client/tests/jest/lib/database/withDatabaseConnection.test.ts
@@ -1,12 +1,27 @@
+import {withDatabaseConnection} from "@/lib/database/withDatabaseConnection";
 
+describe('withDatabaseConnection performs resiliently', () => {
 
-describe('withDatabaseConnection performs resiliently', ()=>{
+    afterEach(() => {
+        jest.resetModules()
+    })
 
-    it('throws when missing environmental variables for database access', ()=>{
-        jest.mock('process')
-        process.env.DB_USER.mockResolvedValue(undefined)
-        process.env.DB_PASS.mockResolvedValue(undefined)
-        expect(withDatabaseConnection((connection)=>{}), "", "").toThrow()
+    it('throws when missing environmental variables for database access', async () => {
+        const originalUser = process.env.DB_USER
+        const originalPass = process.env.DB_PASS
+        process.env.DB_USER = ""
+        process.env.DB_PASS = ""
+        await expect(withDatabaseConnection((connection) => {}, "", ""))
+            .rejects.toThrow("Missing environmental variable for database User or Password")
+
+        // Reset variables
+        process.env.DB_USER = originalUser
+        process.env.DB_PASS = originalPass
+    })
+
+    it('resets environmental variables', async () => {
+        await expect(withDatabaseConnection((connection) => {}, "", ""))
+            .rejects.not.toThrow("Missing environmental variable for database User or Password")
     })
 
 })
